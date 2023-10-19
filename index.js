2002dea9e289c6b540d7749827e757fa72e19ad0
theAbiola <64592361+theAbiola@users.noreply.github.com>
2023-10-19 15:03:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Step 1: Make sure that when a user visits the home page,
-//   it shows a random activity.You will need to check the format of the
-//   JSON data from response.data and edit the index.ejs file accordingly.
-
-/* The following lines of code are the ones we used while completing the task of this module
-I had to personally re-write my own code because i think that's how it should be from a user's perspective.
-
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
@@ -37,14 +30,7 @@ res.render("index.ejs", {});
 
 app.post("/", async (req, res) => {
 
-  // Step 2: Play around with the drop downs and see what gets logged.
-  // Use axios to make an API request to the /filter endpoint. Making
-  // sure you're passing both the type and participants queries.
-  // Render the index.ejs file with a single *random* activity that comes back
-  // from the API request.
-  // Step 3: If you get a 404 error (resource not found) from the API request.
-  // Pass an error to the index.ejs to tell the user:
-  // "No activities that match your criteria."
+
   try {
   console.log(req.body);
   const type = req.body.type;
